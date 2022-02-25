e69b94ecd256bb8d521247f13c02dc9a71f6e6e5
aming-r <aming_java@163.com>
2022-02-25 13:33:43 +0800

    Changes

--- src/main/java/com/Controller/FileUploadController.java
+++ src/main/java/com/Controller/FileUploadController.java
@@ -1,12 +1,11 @@
 package com.Controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
@@ -17,8 +16,8 @@ import java.util.UUID;
 @RestController
 public class FileUploadController {
     SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd");
-    @PostMapping("/upload")
-    public String upload(MultipartFile uploadFile, HttpServletRequest request){
+    @RequestMapping("/upload")
+    public String upload(MultipartFile uploadFile, HttpServletRequest request) {
         String realPath = request.getSession().getServletContext().getRealPath("/uploadFile/");
         String format = simpleDateFormat.format(new Date());
         File folder = new File(realPath+format);
